534e299bbdfcf590ebbf44e221982288f64eaecd
Justin Pugh <justypugh@icloud.com>
2020-03-24 14:19:00 -0700

    refactor using context and gave delete button functionality

--- src/Note/Note.js
+++ src/Note/Note.js
@@ -1,31 +1,69 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ApiContext from '../ApiContext';
+import config from '../config';
 import './Note.css';
 
-export default function Note(props) {
-    return (
-        <div className='Note'>
-            <h2 className='Note__title'>
-                <Link to={`/note/${props.id}`}>
-                    {props.name}
-                </Link>
-            </h2>
-            <button className='Note__delete' type='button'>
-            <FontAwesomeIcon icon='trash-alt' />
-            {' '}
-            Remove
-            </button>
-            <div className='Note__dates'>
-                <div className='Note__dates-modified'>
-                    Modified
-                    {' '}
-                    <span className='Date'>
-                        {new Date(props.modified).toLocaleDateString()} &nbsp;
-                        {new Date(props.modified).toLocaleTimeString()}
-                    </span>
+export default class Note extends React.Component {
+    static defaultProps = {
+        onDeleteNote: () => {},
+    }
+    static contextType = ApiContext;
+
+    handleClickDelete = e => {
+        e.preventDefault()
+        const noteId = this.props.id
+
+        fetch(`${config.API_ENDPOINT}/notes/${noteId}`, {
+            method: 'DELETE',
+            headers: {
+                'content-type': 'application/json'
+            },
+        })
+        .then(res => {
+            if (!res.ok)
+                return res.json().then(e => Promise.reject(e))
+            return res.json()
+        })
+        .then(() => {
+            this.context.deleteNote(noteId)
+            // allow parent to perform extra behavior
+            this.props.onDeleteNote(noteId)
+        })
+        .catch(error => {
+            console.log({error})
+        })
+    }
+    render() {
+        const { name, id, modified } = this.props
+        return (
+            <div className='Note'>
+                <h2 className='Note__title'>
+                    <Link to={`/note/${id}`}>
+                        {name}
+                    </Link>
+                </h2>
+                <button 
+                    className='Note__delete' 
+                    type='button'
+                    onClick={this.handleClickDelete}
+                >
+                <FontAwesomeIcon icon='trash-alt' />
+                {' '}
+                Remove
+                </button>
+                <div className='Note__dates'>
+                    <div className='Note__dates-modified'>
+                        Modified
+                        {' '}
+                        <span className='Date'>
+                            {new Date(modified).toLocaleDateString()} &nbsp;
+                            {new Date(modified).toLocaleTimeString()}
+                        </span>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
